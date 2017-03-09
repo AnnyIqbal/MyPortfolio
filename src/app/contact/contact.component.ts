0468a83e6bb6159d18bff4bee20c773edb49d4dd
Anny <quratulann_iqbal@hotmail.com>
2017-03-09 09:19:12 +0500

    wip about add udemy contact social media plus firebase db

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -12,6 +12,13 @@ export class ContactComponent implements OnInit {
 
   ContactForm: FormGroup;
 
+    media = [
+      {src: '../../assets/Images/email.png', href: 'mailto:quratulann_iqbal@hotmail.com', class: 'ion-social-facebook', color: 'lightblue'},
+      {src: '../../assets/Images/github.png', href: 'https://github.com/AnnyIqbal', class: 'ion-social-github', color: 'lightgreen'},
+      {src: '../../assets/Images/linkedin.png', href: 'https://pk.linkedin.com/in/qurat-ul-ann-iqbal-aa4a4938', class: 'ion-social-linkedin', color: 'lightpink'},
+      {src: '../../assets/Images/facebook.png', href: 'https://www.facebook.com/quratulann.iqbal', class: 'ion-email', color: '#DDBDF1'}
+    ];
+
   constructor(private route: Router, private formBuilder: FormBuilder) {
      this.ContactForm = this.formBuilder.group({
         name:  ['', Validators.compose([ Validators.required, Validators.maxLength(20), Validators.minLength(3) ])],
