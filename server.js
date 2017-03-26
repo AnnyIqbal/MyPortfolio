8d5d79effb2dfd2b1c8594fe99e62b7be011d356
Anny <quratulann_iqbal@hotmail.com>
2017-03-27 01:38:46 +0500

    added server but it only shows loading...

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-var server = http.createServer(function(req, res) {
-    if(req.url === '/') {
-        res.writeHead(202, {'Content-Type': 'text/html'});
-        fs.readFile(`src/index.html`, 'utf8', function(err, data) {
-            // if(err) throw err;
-            console.log(data);
-            res.end(data);
-        });
-    }
-    // res.end('abc');
+// var server = http.createServer(function(req, res) {
+//     if(req.url === '/') {
+//         res.writeHead(202, {'Content-Type': 'text/html'});
+//         fs.readFile(`dist/index.html`, 'utf8', function(err, data) {
+//             // if(err) throw err;
+//             console.log(data);
+//             res.end(data);
+//         });
+//     }
+//     // res.end('abc');
+// });
+// server.listen(3000, function() {
+//     console.log('server is running on port 3000');
+// });
+
+
+var express = require('express');
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/src/index.html');
 });
-server.listen(3000, function() {
-    console.log('server is running on port 3000');
+app.get('/about', function(req, res) {
+    res.send('this is about page');
 });
+
+app.listen(3000); // Loading ...
